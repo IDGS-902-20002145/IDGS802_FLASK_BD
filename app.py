b514cd08156540f01bc0b361b4a11026e4e91b06
IDGS-802-20002145 <20002145@alumnos.utleon.edu.mx>
2023-03-06 18:54:12 -0600

    Commit clase 06/03/23
    ABCompleto y decorador para el ABCompleto y modificar

--- app.py
+++ app.py
@@ -25,6 +25,34 @@ def index():
         db.session.commit()
     return render_template('index.html',form=create_form)
 
+@app.route("/modificar",methods=['GET','POST'])
+def modificar():
+    create_fprm=forms.UserForm(request.form)
+    if request.method=='GET':
+        id=request.args.get('id')
+        alum1=db.session.query(Alumnos).filter(Alumnos.id==id).first()
+        create_fprm.id.data=request.args.get('id')
+        create_fprm.nombre.data=alum1.nombre
+        create_fprm.apellidos.data=alum1.apellidos
+        create_fprm.email.data=alum1.email
+    if request.method=='POST':
+        id=create_fprm.id.data
+        alum=db.session.query(Alumnos).filter(Alumnos.id==id).first()
+        alum.nombre=create_fprm.nombre.data
+        alum.apellidos=create_fprm.apellidos.data
+        alum.email=create_fprm.email.data
+        db.session.add(alum)
+        db.session.commit()
+        return redirect(url_for('ABCompleto'))
+    return render_template('modificar.html', form= create_fprm)
+
+@app.route("/ABCompleto",methods=["GET","POST"])
+def ABCompleto():
+    create_form=forms.UserForm(request.form)
+    alumno=Alumnos.query.all()
+    
+    return render_template("ABCompleto.html",form=create_form,alumno=alumno)
+
 if __name__=='__main__':
     csrf.init_app(app)
     db.init_app(app)
